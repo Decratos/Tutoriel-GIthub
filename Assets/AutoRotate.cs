95f54ef19965316b6915ee72fb8373d8d26ab92e
Théo Pinto <decratosgames@gmail.com>
2020-12-16 00:23:06 +0100

    Base file
    test

---
+++ Assets/AutoRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotate : MonoBehaviour
+{
+    [SerializeField] private Vector3 rotation;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotation);
+    }
+}
